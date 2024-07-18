674fc3063a2b99d9d208055eee298e195e80f15e
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2024-07-18 14:56:00 -0300

    criacao de funcoes em alguns botoes

--- App.js
+++ App.js
@@ -7,82 +7,186 @@ import {
   TextInput,
 } from "react-native";
 
+import { useState } from "react";
+
 export default function App() {
+  const [valor, setValor] = useState("");
+
+  function execultarOperacao() {
+    setValor(eval(valor));
+  }
+
+  function apagarUmDigito() {
+    setValor(valor.slice(0, -1));
+  }
+
+  function apagarTudo(){
+    setValor('')
+  }
   return (
     <View style={styles.container}>
       <View style={styles.calculator}>
         <View>
-          <TextInput></TextInput>
+          <TextInput
+            style={styles.tela}
+            placeholder="0"
+            onChangeText={setValor}
+            value={valor}
+          ></TextInput>
         </View>
 
         <View style={styles.teclado}>
           <View style={styles.row}>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity style={styles.number} onPress={apagarTudo}>
               <Text style={styles.textButton}>AC</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.number}>
               <Text style={styles.textButton}>( )</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.number}>
-              <Text style={styles.textButton}>%</Text>
+              <Text
+                style={styles.textButton}
+                onPress={() => {
+                  setValor(valor + "%");
+                }}
+              >
+                %
+              </Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.number}>
-              <Text style={styles.textButton}>÷</Text>
+              <Text
+                style={styles.textButton}
+                onPress={() => {
+                  setValor(valor + "÷");
+                }}
+              >
+                ÷
+              </Text>
             </TouchableOpacity>
           </View>
           <View style={styles.row}>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "7");
+              }}
+            >
               <Text style={styles.textButton}>7</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "8");
+              }}
+            >
               <Text style={styles.textButton}>8</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "9");
+              }}
+            >
               <Text style={styles.textButton}>9</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
-              <Text style={styles.textButton}>X</Text>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "*");
+              }}
+            >
+              <Text style={styles.textButton}>*</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.row}>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "4");
+              }}
+            >
               <Text style={styles.textButton}>4</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "5");
+              }}
+            >
               <Text style={styles.textButton}>5</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "6");
+              }}
+            >
               <Text style={styles.textButton}>6</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "-");
+              }}
+            >
               <Text style={styles.textButton}>-</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.row}>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "1");
+              }}
+            >
               <Text style={styles.textButton}>1</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "2");
+              }}
+            >
               <Text style={styles.textButton}>2</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "3");
+              }}
+            >
               <Text style={styles.textButton}>3</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "+");
+              }}
+            >
               <Text style={styles.textButton}>+</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.row}>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + "0");
+              }}
+            >
               <Text style={styles.textButton}>0</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
-              <Text style={styles.textButton}>,</Text>
+            <TouchableOpacity
+              style={styles.number}
+              onPress={() => {
+                setValor(valor + ".");
+              }}
+            >
+              <Text style={styles.textButton}>.</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity style={styles.number} onPress={apagarUmDigito}>
               <Text style={styles.textButton}>←</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.number}>
+            <TouchableOpacity style={styles.number} onPress={execultarOperacao}>
               <Text style={styles.textButton}>=</Text>
             </TouchableOpacity>
           </View>
@@ -95,27 +199,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   calculator: {
     backgroundColor: "#000000",
     alignItems: "center",
-    justifyContent: "center",
-    width: 300,
-    height: 400,
-    margin: "auto",
+    justifyContent: "flex-end",
+    width: "auto",
+    height: 500,
+    padding: 15,
     borderRadius: 15,
   },
   teclado: {
     flexDirection: "column",
-    // backgroundColor: "#0ff0ff",
     padding: 10,
   },
 
   number: {
-    width: 40,
-    height: 40,
+    width: 50,
+    height: 50,
     backgroundColor: "#52a3eb",
-    borderRadius: 5,
+    borderRadius: 50,
     alignItems: "center",
     justifyContent: "center",
     margin: 5,
@@ -124,10 +229,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     color: "#ffffff",
+    fontSize: 20,
+    textAlign: "center",
   },
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
-    // marginBottom: 
+  },
+  tela: {
+    // backgroundColor: 'white',
+    width: 225,
+    height: 50,
+    fontSize: 50,
+    color: "white",
+    textAlign: "right",
+    fontWeight: "regular",
   },
 });
